f7cc9c073ab9c457645b35046c547981079c4ce1
carlo <jos.paredesr@duocuc.cl>
2025-11-12 21:44:05 -0300

    Avances

--- app/src/main/java/com/example/apprecetas/MainActivity.kt
+++ app/src/main/java/com/example/apprecetas/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.apprecetas
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
+        val btnBuscar: FloatingActionButton = findViewById(R.id.btn_buscar)
+        val btnFavoritos: FloatingActionButton = findViewById(R.id.btn_favoritos)
+
+
+        btnBuscar.setOnClickListener {
+            val intent = Intent(this, BuscarActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFavoritos.setOnClickListener {
+            // Prepara un intent para ir a la FavoritosActivity
+            val intent = Intent(this, FavoritosActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
